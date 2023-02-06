84186779bde1fa2078b161e541641ddfe5938bde
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-06 10:02:53 -0700

    Save 2

--- linear-search.js
+++ linear-search.js
@@ -1,17 +1,9 @@
-function linearSearch(array, Searchterm) {
-	const numbers = [3, 7, 12, 15, 19, 23, 28, 30, 34, 40];
-	for (var i = 0; i < array.length; i++) {
-		if (arr[19] === item) {
-			return 19;
+function linearSearch(arr, Searchterm) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === Searchterm) {
+			return i;
 		}
 	}
 	return -1
 }
 module.exports = linearSearch;
-
-/*
-	 ** Check each element from start to finish
-	 ** until you find an element that matches the search term
-	 **
-	 ** Return the index of the found element
-	 */

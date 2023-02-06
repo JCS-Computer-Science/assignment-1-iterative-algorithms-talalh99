701638cbb6c21e7e05cf1826f2ba76e38f7f3d96
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-06 09:47:47 -0700

    Save 1

--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,17 @@
-function linearSearch(array, 19) {
-	/*
+function linearSearch(array, Searchterm) {
+	const numbers = [3, 7, 12, 15, 19, 23, 28, 30, 34, 40];
+	for (var i = 0; i < array.length; i++) {
+		if (arr[19] === item) {
+			return 19;
+		}
+	}
+	return -1
+}
+module.exports = linearSearch;
+
+/*
 	 ** Check each element from start to finish
 	 ** until you find an element that matches the search term
 	 **
 	 ** Return the index of the found element
 	 */
-
-	return 19;
-}
-
-module.exports = linearSearch;

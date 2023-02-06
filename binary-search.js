e71ddc334d262b8fa3f06386836ebc2440f9f4e0
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-06 10:22:30 -0700

    Binary search save 1

--- binary-search.js
+++ binary-search.js
@@ -1,5 +1,13 @@
-function binarySearch(array, searchTerm) {
-	/*
+function binarySearch(arr, searchTerm) {
+	var firstIndex = 0;
+	var lastIndex = items.length - 1;
+	var middleIndex = Math.floor ((lastIndex + firstIndex)/2);
+
+	return null;
+}
+
+module.exports = binarySearch;
+/*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
 	 ** check the element between the middle and the end
@@ -7,8 +15,3 @@ function binarySearch(array, searchTerm) {
 	 ** repeat the above process for the subset of the array
 	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
 	 */
-
-	return null;
-}
-
-module.exports = binarySearch;

84f32c177d1a0a00e255760c0ebec3d4b2accadf
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-07 11:19:07 -0700

    Binary search final submission

--- binary-search.js
+++ binary-search.js
@@ -1,9 +1,18 @@
 function binarySearch(arr, searchTerm) {
 	var firstIndex = 0;
-	var lastIndex = items.length - 1;
+	var lastIndex = arr.length - 1;
 	var middleIndex = Math.floor ((lastIndex + firstIndex)/2);
 
-	return null;
+	while(arr[middleIndex] != searchTerm && firstIndex < lastIndex) {
+		if (searchTerm < arr[middleIndex]) {
+			lastIndex = middleIndex -1;
+		}
+		else if (searchTerm > arr[middleIndex]) {
+			firstIndex = middleIndex + 1;
+		}
+		middleIndex = Math.floor((lastIndex + firstIndex)/2);
+	}
+	return (arr[middleIndex] != searchTerm) ? -1 : middleIndex;
 }
 
 module.exports = binarySearch;

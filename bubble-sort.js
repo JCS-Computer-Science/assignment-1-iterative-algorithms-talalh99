cce6cc0868dced58b4aae3e8372da6179e8e261d
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-08 10:24:14 -0700

    Bubble sort save one

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,5 +1,19 @@
-function bubbleSort(arr) {
-	/*
+function bubbleSort(numberArr, size) {
+    for (var lastIndex = size - 1; lastIndex > 0; lastIndex--) {
+        for (var i = 0; i < lastIndex; i++) {
+            if (numberArr[i] > numberArr[i + 1]) {
+                var temp = numberArr[i];
+                numberArr[i] = numberArr[i + 1];
+                numberArr[i + 1] = temp;
+            }
+        }
+    }
+    return numberArr;
+}
+var arr = [14, -25, 65, 19, 23, 4];
+console.log(bubbleSort(arr, arr.length));
+module.exports = bubbleSort;
+/*
     ** Compare the first two elements and swap if necessary.
     ** Then compare the second and third elements and swap if
     ** necessary. Continue until you've checked and swapped (if needed)
@@ -9,6 +23,17 @@ function bubbleSort(arr) {
     ** cycle without needing any swaps
     */
 
-	return arr;
-}
-module.exports = bubbleSort;
+/* 
+    **let isSwapped = true
+    while(isSwapped) {
+        isSwapped = false
+        arr.forEach((item, index) => {
+            if(item > arr[index +1]) {
+                arr[index] = arr[index + 1]
+                arr[index + 1] = item
+                isSwapped = true
+            }
+        })
+        return arr
+    }
+*/

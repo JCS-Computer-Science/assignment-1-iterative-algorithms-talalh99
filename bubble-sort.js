051b1212cac966ca6015aa389633395a9b22de86
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-10 09:57:22 -0700

    bubble sort save 3

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,13 +1,17 @@
-function bubbleSort(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      for (let j = 0; j < i; j++) {
-        if (arr[j] > arr[j + 1]) {
-          ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-        }
+function bubbleSort(ar) {
+  for (var i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]; 
+        arr[j] = arr[j + 1];
+        arr [j + 1] = temp;
       }
     }
-    return arr
   }
+  return arr;
+}
+
+
 /*
     ** Compare the first two elements and swap if necessary.
     ** Then compare the second and third elements and swap if
@@ -17,4 +21,13 @@ function bubbleSort(arr) {
     ** Repeat the above process until you get through an entire
     ** cycle without needing any swaps
     */
+/* for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr
+*/
 

1b7fecd6dfbb0b971a1c2c3a66d22247a90d5951
Talal Habib <65240001@edu.cbe.ab.ca>
2023-02-13 09:13:29 -0700

    Bubble Sort Final Submission

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,33 +1,14 @@
-function bubbleSort(ar) {
+function bubbleSort(arr) {
   for (var i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j]; 
-        arr[j] = arr[j + 1];
-        arr [j + 1] = temp;
+    for (var j = 0; j < (arr.length - i -1); j++) {
+      if (arr[j] > arr[j+1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j+1] = temp
       }
     }
-  }
-  return arr;
-}
-
-
-/*
-    ** Compare the first two elements and swap if necessary.
-    ** Then compare the second and third elements and swap if
-    ** necessary. Continue until you've checked and swapped (if needed)
-    ** the last and second last elements.
-    
-    ** Repeat the above process until you get through an entire
-    ** cycle without needing any swaps
-    */
-/* for (let i = arr.length - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-      }
-    }
+    console.log(arr)
   }
   return arr
-*/
-
+}
+module.exports = bubbleSort;
